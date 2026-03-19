c3c7500da983c08c7cc8b5c4e83263c66688b56c
BrandonWang269 <brandonw269@nycstudents.net>
2026-03-19 10:16:52 -0400

    Second

--- test.py
+++ test.py
@@ -1,9 +1,32 @@
-import json
+""" import json
 pokedex = open("./pokedex.json", encoding="utf8")
-data = json.load(pokedex)
+data = json.load(pokedex) """
 
 """ print(data[0]) """
 
 """ for index, item in enumerate(data):
     print(index, ":", item["name"]) """
 
+""" import json
+pokedex = open("./pokedex.json", encoding="utf8")
+data = json.load(pokedex) """
+""" choice = input("Pick a language from chinese, english, japanese, french: ")
+for index, item in enumerate(data):
+    if choice == "chinese":
+        print(f"{index}: {item["name"]["chinese"]}")
+    if choice == "english":
+        print(f"{index}: {item["name"]["english"]}")
+    if choice == "japanese":
+        print(f"{index}: {item["name"]["japanese"]}")
+    if choice == "french":
+        print(f"{index}: {item["name"]["french"]}") """
+
+import json
+pokedex = open("./pokedex.json", encoding="utf8")
+data = json.load(pokedex)
+choice = input("What type do you want: ")
+for index, item in enumerate(data):
+    if choice == "Bug":
+        print(f"{index}: {item["type"]["Bug"]}")
+    else:
+        print("nothing")
